2547af121815c2d7135992881ece4c317dbc5e1e
Kiril Klein <kleinkiril94@gmail.com>
2021-08-31 11:17:26 +0200

    requirements.txt added, trying to convert dicom2nifti using matlab

--- Cobra/data_analysis.py
+++ Cobra/data_analysis.py
@@ -7,7 +7,7 @@ Created on Mon Aug 30 10:32:43 2021
 
 import data_access.load_data_tools as ld
 import os
-from vis import display3d
+from vis import vis
 import importlib
 from pydicom import dcmread
 from utils import utils
@@ -15,30 +15,38 @@ importlib.reload(ld)
 
 
 def p(string): print(string)
-# In[Specify Directories]
+# In[main directories]
 
 base_data_dir = "Z:"
 data_dirs = os.listdir(base_data_dir)
-#all_dirs = [x[0] for x in os.walk(base_data_dir)]
 positive_dir = f"{base_data_dir}/positive"
-healthy_dirs = [os.path.join(base_data_dir, x) for x in data_dirs if x.startswith('2019')]
-
-#healthy_patients_dir_month01 = [os.path.join(healthy_dirs[0], x) for x in os.listdir(healthy_dirs[0])]
-#example_healthy_patient_dir_month01 = "Z:/2019_01/00ccc73189d43bb9f4123d92ea3f19c1"
-#patient00_scans_dirs = [os.path.join(patient00_dir, x) for x in os.listdir(patient00_dir)]
-#print(patient00_scans_dirs)
-
-
+healthy_dirs = [f"Z:/{x}" for x in data_dirs if x.startswith('2019')]
+print(healthy_dirs)
 # In[Look at one patient with subdirectories]
 pos_patient_dir = "Z:\\positive\\f430460b276e6618ad2c73daf269c228"
 patient0 = ld.Patient(pos_patient_dir)
-#p(patient0.get_scan_directories())
 #arr = patient0.reconstruct3d(patient0.get_scan_directories()[0])
 patient0.show_scan(1, {'axis':0})
 
 
-
+# In[]
+def count_subdirectories(dir_):
+    return sum(os.path.isdir(os.path.join(dir_,x)) for x \
+               in os.listdir(dir_))
 
 # In[]
-pos_patient_directories = utils.list_subdir(positive_dir)
-p(pos_patient_directories)
+healthy_count = 0
+for subdir in healthy_dirs:
+    healthy_count += count_subdirectories(subdir)
+    print(healthy_count)
+print(healthy_count)
+
+
+
+
+
+
+
+
+
+
