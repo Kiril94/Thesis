5efe6e6adbe9218c227b7891117f97ceb2b25dba
Kiril Klein <kleinkiril94@gmail.com>
2021-09-01 16:54:25 +0200

    minor

--- Cobra/data_analysis.py
+++ Cobra/data_analysis.py
@@ -42,16 +42,8 @@ path = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104.json"
 with open(os.path.join(out_path, path), 'r') as f:
 	data = json.load(f)
 print(data)
-# In[Di2nii]
-def convert_dcm_2_nii_x(dcm_folder, output_folder):
-    converter = Dcm2niix()
-    converter.inputs.source_dir = dcm_folder
-    converter.inputs.output_dir = output_folder
-    converter.inputs.compress = 'i'
-    converter.run()
 
 # In[main directories]
-
 base_data_dir = "Z:/"
 data_dirs = os.listdir(base_data_dir)
 positive_dir = f"{base_data_dir}/positive" 
@@ -68,37 +60,7 @@ p0_scandir = p0.get_scan_directories()
 #patient_ids = [p0.scan_dictionary(n, reconstruct_3d=False).PatientID \
 #              for n in range(len(p0_scandir))]
 print(p0_scandir[2])
-# In[try dicom2nifti]
-d2n.convert_directory(p0_scandir[0], "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_python")
-# In[try new script to convert]
-convert_dcm_2_nii_x(p0_scandir[0], "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_matlab")
-# In[]
-scan_dir = "Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475dad00723fb0ca7\\MR\\2c76b30765e19a46b140d0d07df70bb5"
-file_dir = utils.list_subdir(scan_dir)[0]
-file = pydicom.read_file(file_dir)
-print(file)
 
-# In[Try dcmstack]
-src_dcms = utils.list_subdir(p0_scandir[0])
-stacks = dcmstack.parse_and_stack(src_dcms)
-# In[]
-#help(dcmstack.parse_and_stack)
-print(dir(stacks))
-#print(stacks.values())
-stack = stacks.values()
-nii = stack.to_nifti()
-nii.to_filename("D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_py\\test.nii")
-# In[]
-#p0_dicomdir = os.path.join(p0_scandir[0], os.listdir(p0_scandir[0])[0])
-for sd, cd in zip(p0_scandir, patient_ids):
-    print(f"scan dir: {sd[-5:]}, date:{cd}")
-    #p(f"date: {cd.month} {cd.day}")
-#print(p0_scandir)
-#print(os.path.listdir(pos_patient_dir))
-# In[convert di2nifti]
-dicom_dir = "Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475dad00723fb0ca7\\MR\\2c76b30765e19a46b140d0d07df70bb5"
-nifti_out_dir = "D:/Thesis/Cobra/data/dicom2nifti/p0"
-dicom2nifti.dicom2nifti(dicom_dir, nifti_out_dir)
 # In[look at ni header]
 path_roi = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104a_ROI1.nii"
 path_all = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104.nii"
@@ -107,9 +69,6 @@ img_mat = nib.load(nii_path)
 data = img_mat.get_fdata()
 print(data.shape)
 vis.display3d(data, axis=1)
-# In[]
-
-
 # In[Count Patients]
 healthy_count = 0
 for subdir in healthy_dirs:
