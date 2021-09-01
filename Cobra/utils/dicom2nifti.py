a0850be12059e759fd39693661b646a098ecde65
Kiril Klein <kleinkiril94@gmail.com>
2021-09-01 17:54:08 +0200

    dcm2niix.exe &co added

--- Cobra/utils/dicom2nifti.py
+++ Cobra/utils/dicom2nifti.py
@@ -8,12 +8,13 @@
 
 import os
 
+dcm2nii_dir = "D:\\Thesis\\Cobra\\helper\\dcm2niix_win\\dcm2niix.exe"
 
 def dcm2nii(dcm_path, out_path):
     """Given dicom path and output path, converts dicom files in 1 folder
     to a nii file + json file containing the header. 
     The files are named corresponding
     to the folder name which is the SOPInstanceUID."""
-    os.system(f"cmd /k D:\\Thesis\\Cobra\\helper\\dcm2niix_win\\dcm2niix.exe \
+    os.system(f"cmd /k  {dcm2nii_dir}\
               -f %f -o {out_path} {dcm_path}")
 
