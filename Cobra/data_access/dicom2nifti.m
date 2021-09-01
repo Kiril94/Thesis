c7e8150ab91bdada64091b7f6f67fbc1377ae5ed
Kiril Klein <kleinkiril94@gmail.com>
2021-09-01 16:51:06 +0200

    new utils added

--- Cobra/data_access/dicom2nifti.m
+++ Cobra/data_access/dicom2nifti.m
@@ -1,3 +1,3 @@
-scan_path = "Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475dad00723fb0ca7\\MR\\2c76b30765e19a46b140d0d07df70bb5";
-nifti_path = "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p0_matlab";
+scan_path = "Z:\\positive\\27092ec9a3fd6f8c36851bd56c8168fd\bfb6b1c2afd6f6a55c8bffdb8fb1f11a\MR\5eda840fa8581fc0f11ca828e0a90675";
+nifti_path = "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_matlab";
 dicm2nii(scan_path, nifti_path, '.nii');
--- Cobra/data_analysis.py
+++ Cobra/data_analysis.py
@@ -11,30 +11,83 @@ from vis import vis
 import importlib
 from pydicom import dcmread
 from utils import utils
-from utils import dicom2nifti
+#from utils import dicom2nifti
+import dicom2nifti as d2n
 import nibabel as nib
-from pydicom import dcmread
-importlib.reload(dicom2nifti)
+import glob
+import pydicom
+#from nipype.interfaces.dcm2nii import Dcm2niix
+import sys
+import json
+import time
+#importlib.reload(dicom2nifti)
+#import dcm2niix
 importlib.reload(ld)
 def p(string): print(string)
+import dcmstack
+
+
+# In[]
+import os
+dcm2niix_dir = "D:\\Thesis\\Cobra\\helper\\dcm2niix_win\\dcm2niix.exe"
+in_path = p0_scandir[0]#"Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475dad00723fb0ca7\\MR\\2c76b30765e19a46b140d0d07df70bb5"
+out_path = "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p3"
+#os.system(f"{dcm2niix_dir} -h")
+start = time.time()
+os.system(f"cmd /k {dcm2niix_dir} -o {out_path} {in_path}")
+end = time.time()
+print(end - start)
+# In[Look at the result]
+path = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104.json"
+with open(os.path.join(out_path, path), 'r') as f:
+	data = json.load(f)
+print(data)
+# In[Di2nii]
+def convert_dcm_2_nii_x(dcm_folder, output_folder):
+    converter = Dcm2niix()
+    converter.inputs.source_dir = dcm_folder
+    converter.inputs.output_dir = output_folder
+    converter.inputs.compress = 'i'
+    converter.run()
+
 # In[main directories]
 
 base_data_dir = "Z:/"
 data_dirs = os.listdir(base_data_dir)
 positive_dir = f"{base_data_dir}/positive" 
 healthy_dirs = [f"{base_data_dir}/{x}" for x in data_dirs if x.startswith('2019')]
-print(data_dirs)
+print(f"main directories: {data_dirs}")
 # In[all positives]
 pos_patients_list = utils.list_subdir(positive_dir)
 # In[Look at one patient with subdirectories]
-pos_patient_dir = pos_patients_list[200]
+pos_patient_dir = pos_patients_list[20]
 p0 = ld.Patient(pos_patient_dir)
 p0_scandir = p0.get_scan_directories()
 #for p in p0_scandir:
 #    print(dcmread(utils.list_subdir(p)[0]).SOPInstanceUID)
 #patient_ids = [p0.scan_dictionary(n, reconstruct_3d=False).PatientID \
 #              for n in range(len(p0_scandir))]
-print(p0.info())
+print(p0_scandir[2])
+# In[try dicom2nifti]
+d2n.convert_directory(p0_scandir[0], "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_python")
+# In[try new script to convert]
+convert_dcm_2_nii_x(p0_scandir[0], "D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_matlab")
+# In[]
+scan_dir = "Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475dad00723fb0ca7\\MR\\2c76b30765e19a46b140d0d07df70bb5"
+file_dir = utils.list_subdir(scan_dir)[0]
+file = pydicom.read_file(file_dir)
+print(file)
+
+# In[Try dcmstack]
+src_dcms = utils.list_subdir(p0_scandir[0])
+stacks = dcmstack.parse_and_stack(src_dcms)
+# In[]
+#help(dcmstack.parse_and_stack)
+print(dir(stacks))
+#print(stacks.values())
+stack = stacks.values()
+nii = stack.to_nifti()
+nii.to_filename("D:\\Thesis\\Cobra\\data\\dicom2nifti\\p1_py\\test.nii")
 # In[]
 #p0_dicomdir = os.path.join(p0_scandir[0], os.listdir(p0_scandir[0])[0])
 for sd, cd in zip(p0_scandir, patient_ids):
@@ -47,28 +100,22 @@ dicom_dir = "Z:\\positive\\00e520dd9e4c7f2b7798263bd0916221\\2d8ef0eb9e77c14475d
 nifti_out_dir = "D:/Thesis/Cobra/data/dicom2nifti/p0"
 dicom2nifti.dicom2nifti(dicom_dir, nifti_out_dir)
 # In[look at ni header]
-
-img = nib.load(nifti_out_dir+'.nii')
-img_mat = nib.load("D:\\Thesis\\Cobra\\data\\dicom2nifti\\p0_matlab\\x_MPR_Thick_Range_4_.nii")
-
-print(img.header)
-print(img_mat.header)
+path_roi = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104a_ROI1.nii"
+path_all = "2c76b30765e19a46b140d0d07df70bb5_MPR_Thick_Range[4]_0_104.nii"
+nii_path = os.path.join(out_path,path_all )
+img_mat = nib.load(nii_path)
+data = img_mat.get_fdata()
+print(data.shape)
+vis.display3d(data, axis=1)
 # In[]
-def count_subdirectories(dir_):
-    return sum(os.path.isdir(os.path.join(dir_,x)) for x \
-               in os.listdir(dir_))
 
-# In[]
+
+# In[Count Patients]
 healthy_count = 0
 for subdir in healthy_dirs:
     healthy_count += count_subdirectories(subdir)
     print(f"subdir: {subdir}, accumulated sum: {healthy_count}")
 print(healthy_count)
-
-# In[]
-print(healthy_dirs)
-# In[]
-print(os.path.exists("Z://2019_08"))
 # In[]
 
 
