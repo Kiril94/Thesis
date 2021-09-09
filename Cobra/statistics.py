aa9124a887edd84e2c2ebc722ce77308739e4841
Kiril Klein <kleinkiril94@gmail.com>
2021-09-09 16:10:53 +0200

    minor changes to funcs, masks added

--- Cobra/statistics.py
+++ Cobra/statistics.py
@@ -28,16 +28,22 @@ def mask_sequence_type(df, str_):
     mask = df['SeriesDescription'].str.contains(str_, na=False)
     return mask
 
-def check_sequence_flags(df, flags):
-    """calls mask_sequence type for a list of flags and combines
+def check_sequence_tags(df, flags):
+    """calls mask_sequence type for a list of tags and combines
     the masks with or"""
     masks = []
-    for flag in flags:
-        masks.append(mask_sequence_type(df, flag))
+    for tag in tags:
+        masks.append(mask_sequence_type(df, tag))
     mask = masks[0]
     for i in range(1, len(masks)):
         mask = mask | masks[i]
     return mask
+# In[Test functions]
+masks = [np.array([1, 0, 0]), np.array([0, 0, 0]), np.array([1, 1, 0])]
+mask = masks[0]
+for i in range(1, len(masks)):
+    mask = mask | masks[i]
+print(mask)
 # In[tables directories]
 script_dir = os.path.realpath(__file__)
 base_dir = Path(script_dir).parent
@@ -48,31 +54,40 @@ pos_tab_dir = f"{table_dir}/pos.csv"
 df_p = pd.read_csv(pos_tab_dir, encoding= 'unicode_escape')
 keys = df_p.keys()
 p(keys)
-# In[relevant interest]
-gadolinium_f = ['dotarem', 'Gd','gd', 'GD', 'Dotarem', 
-                    'Gadolinium', 'Dotarem']
-t1_f = ['T1', 't1']
-mprage_f = ['mprage', 'MPRAGE']
-tfe_f = ['tfe', 'TFE']
-fspgr_f = ['FSPGR']
-flair_f = ['FLAIR','flair']
-fse_f = ['FSE', 'fse']
-gre_f  = ['GRE', 'gre']
-t2s_f = ['T2\*']
-dti_f = ['DTI', 'dti']
-swi_f = ['SWI', 'swi']
-# In[]
-T1_mask = df_p['SeriesDescription'].str.contains('swi', na=False)
+
+# In[relevant tags]
+t1_t = ['T1', 't1']
+mprage_t = ['mprage', 'MPRAGE', 'mpr', 'MPR']
+tfe_t = ['tfe', 'TFE']
+fspgr_t = ['FSPGR']
+
+flair_t = ['FLAIR','flair']
+
+t2_t = ['T2', 't2']
+fse_t = ['FSE', 'fse', '']
+
+t2s_t = ['T2\*', 't2\*']
+gre_t  = ['GRE', 'gre']
+
+dti_t = ['DTI', 'dti']
+
+swi_t = ['SWI', 'swi']
+dwi_t = ['DWI', 'dwi']
+gadolinium_t = ['dotarem', 'Dotarem', 'Gd','gd', 'GD', 'Gadolinium']
+
+# In[Get corresponding masks]
+
+# In[Count flags]
+t1_m = check_sequence_flags(df_p, t1_t)
+t2_m = check_sequence_flags(df_p, t2_t)
+t1_c = np.count_nonzero(check_sequence_flags(df_p, t1_t))
+t1_mprage_c = t1_c
+print(t1_c)
+
+# In[tests]
+T1_mask = df_p['SeriesDescription'].str.contains('\+gd', na=False)
 #FLAIR_mask = df_p['SeriesDescription'].str.contains('FLAIR', na=False)
-
 print(df_p[T1_mask].SeriesDescription)
-
-# In[Keys]
-
-mprage_flags = []
-t1_mask = df_p['SeriesDescription'].str.contains('t1', na=False)
-print(df_p[t1_mask].SeriesDescription)
-
 # In[Questions]
 # whats FGRE
 # 
