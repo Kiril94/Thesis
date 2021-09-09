196a11a47f2152fdcc897b112c74bd15342b1fd4
Kiril Klein <kleinkiril94@gmail.com>
2021-09-09 15:59:50 +0200

    some helper functions added

--- Cobra/statistics.py
+++ Cobra/statistics.py
@@ -10,14 +10,34 @@ import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 import numpy as np
+
+# In[Define some helper functions]
 def p(x):
     print(x)
 
 def only_first_true(a, b):
     """takes two binary arrays
-    and returns only elements as True
-    where only the el. of the first array is true"""
+    and returns True
+    where only the el. of the first array is true
+    if only the second or both are true returns false"""
     return a&np.logical_not(a&b)
+
+def mask_sequence_type(df, str_):
+    """Checks in series description if it
+    contains str_"""
+    mask = df['SeriesDescription'].str.contains(str_, na=False)
+    return mask
+
+def check_sequence_flags(df, flags):
+    """calls mask_sequence type for a list of flags and combines
+    the masks with or"""
+    masks = []
+    for flag in flags:
+        masks.append(mask_sequence_type(df, flag))
+    mask = masks[0]
+    for i in range(1, len(masks)):
+        mask = mask | masks[i]
+    return mask
 # In[tables directories]
 script_dir = os.path.realpath(__file__)
 base_dir = Path(script_dir).parent
@@ -27,6 +47,7 @@ table_dir = f"{base_dir}/tables"
 pos_tab_dir = f"{table_dir}/pos.csv" 
 df_p = pd.read_csv(pos_tab_dir, encoding= 'unicode_escape')
 keys = df_p.keys()
+p(keys)
 # In[relevant interest]
 gadolinium_f = ['dotarem', 'Gd','gd', 'GD', 'Dotarem', 
                     'Gadolinium', 'Dotarem']
@@ -34,21 +55,24 @@ t1_f = ['T1', 't1']
 mprage_f = ['mprage', 'MPRAGE']
 tfe_f = ['tfe', 'TFE']
 fspgr_f = ['FSPGR']
+flair_f = ['FLAIR','flair']
+fse_f = ['FSE', 'fse']
+gre_f  = ['GRE', 'gre']
+t2s_f = ['T2\*']
+dti_f = ['DTI', 'dti']
+swi_f = ['SWI', 'swi']
+# In[]
+T1_mask = df_p['SeriesDescription'].str.contains('swi', na=False)
+#FLAIR_mask = df_p['SeriesDescription'].str.contains('FLAIR', na=False)
 
-# In[]
-T1_mask = np.array(
-    df_p['SeriesDescription'].str.contains('IR', na=False))
-FLAIR_mask = np.array(
-    df_p['SeriesDescription'].str.contains('FLAIR', na=False))
-print(T1_mask & FLAIR_mask)
 print(df_p[T1_mask].SeriesDescription)
-# In[]
-a = np.array([1, 0, 1, 0])
-b = np.array([1, 0, 0, 1])
-print(a&np.logical_not(a&b))
 
 # In[Keys]
 
 mprage_flags = []
 t1_mask = df_p['SeriesDescription'].str.contains('t1', na=False)
 print(df_p[t1_mask].SeriesDescription)
+
+# In[Questions]
+# whats FGRE
+# 
