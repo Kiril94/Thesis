b364d7fe1691b5cbbcfcb8fd617a502d5f62421e
NeusRodejaFerrer <neus@rodeja.me>
2021-09-10 10:20:37 +0200

    change dicom2nift

--- Cobra/utilss/dicom2nifti.py
+++ Cobra/utilss/dicom2nifti.py
@@ -14,18 +14,18 @@ base_dir = Path(script_dir).parent.parent
 dcm2nii_exe_path = os.path.join(base_dir, "helper\\dcm2niix_win\\dcm2niix.exe")
 
 
-def dcm2nii(dcm_path, out_path, compression=6):
+def dcm2nii(dcm_path, out_path, compression=6,op_sys=0):
     """Given dicom path and output path, converts dicom files in 1 folder
     to a nii file + json file containing the header. 
     The files are named corresponding
     to the folder name which is the SOPInstanceUID."""
-
-    #---------------------------WINDOWS
-    #os.system(f"cmd /k  {dcm2nii_exe_path}\ 
-        #-f %f -o {out_path} {dcm_path}") 
-    #---------------------------LINUX
-    os.system(f'dcm2niix -w 0 -{compression} -f %f -o {out_path} {dcm_path}')
-
-    os.system(f"cmd /k {dcm2nii_exe_path} -{compression}\
+    if (op_sys==0): #WINDOWS
+            os.system(f"cmd /k {dcm2nii_exe_path} -{compression}\
               -f %f_%z -w 0 -o {out_path} {dcm_path}")
+    elif (op_sys==1):  #LINUX  
+        os.system(f'dcm2niix -w 0 -{compression} -f %f -o {out_path} {dcm_path}')
+    else: 
+        raise Exception('Non valid op_sys. Available options are 0 for Windows or 1 for Linux')
+
+
 
