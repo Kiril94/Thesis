e3b08f2e81b20dbdd6daf4c441a361527fa43a47
Kiril Klein <kleinkiril94@gmail.com>
2021-10-13 16:34:17 +0200

    test file added

---
+++ Pipelines/slurm/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 16:15:50 2021
+
+@author: klein
+"""
+
+import numpy as np
+
+a = np.zeros(10)
+b = np.ones(10)
+c = a + b
+np.savetxt('test.txt', c, fmt='%.18e', delimiter=' ', newline='\n', 
+              header='', footer='', comments='# ', encoding=None)
