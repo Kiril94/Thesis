f03beec6b7114e9ce095983668c94e1cd71403d0
Kiril Klein <kleinkiril94@gmail.com>
2021-10-15 14:22:45 +0200

    copying files using sif directory structure

--- Cobra/cobra/sif_to_hdd.py
+++ Cobra/cobra/sif_to_hdd.py
@@ -82,53 +82,26 @@ for pat in patient_list_dfft[last_patient_idx:]:
 dwi_t2s_gre = np.loadtxt(join(download_pat_path, "ge_dwi_t2s_gre.txt"),
                                    dtype='str')
 gdtg = df_all[df_all['PatientID'].isin(dwi_t2s_gre)]
-#df_patients_0 = df_patients_0[df_patients_0['Sequence'].isin(rel_seq)]
 gdtg = gdtg.sort_values('PatientID')
-
 patient_list_gdtg = gdtg.PatientID.unique()
 #last_patient = "15473b3462554d4f81eb36caefca4978"
 #last_patient_idx = np.where(patient_list==last_patient)[0][0]
 #print(patient_list[last_patient_idx:])
-# In[]
-print(volume_dir_df.SeriesInstanceUID)
+batch0 = patient_list_gdtg[:50]
 
-# In[]
+print(len(patient_list_gdtg))
+# In[copy whole tree]
+ge_dir = os.path.normpath("G:\CoBra\Data\GE")
+np.savetxt(join(ge_dir, 'batch_0'), np.array(batch0))
 
-for pat in patient_list_gdtg[:1]:
+for pat in batch0[:1]:
     # Get patient directory
     patient_dir = volume_dir_df[
         stats.check_tags(volume_dir_df, tags=[pat], key='Directory')].iloc[0,1]
-    #print(join(*list(patient_dir.iloc[0,1].split(os.sep)[:2])))
     patient_dir = os.path.normpath(patient_dir)
     patient_dir = join(*list(patient_dir.split(os.sep)[:2]))
-    for doc in glob.iglob(f"Y:/{patient_dir}/*/DOC/*.pdf"):
-        counter = 0
-        doc_dst_file_dir = join("G:/CoBra/Data/test", patient_dir, 'DOC')
-        try:
-            shutil.move(doc, doc_dst_file_dir);
-        except:
-            if counter==1:
-                print(f"Destination path {doc_dst_file_dir} already exists")
-        counter += 1
-    break
-    start = time.time()
-    print(f"patient: {pat}:", end=' ')
-    volumes = gdtg[gdtg.PatientID==pat]['SeriesInstanceUID']
-    print(f"{len(volumes)} volumes")
-    for volume in volumes:
-        volume_dir = volume_dir_dic[volume]
-        counter = 0
-        for dcm_file in glob.iglob(f"Y:/{volume_dir}/*"):
-            counter+=1
-            dst_file_dir = target_path(
-                Path(os.path.split(dcm_file)[0]), Path("G:/CoBra/Data/test"))
-            try:
-                shutil.move(dcm_file, dst_file_dir);
-            except:
-                if counter==1:
-                    print(f"Destination path {dst_file_dir} already exists")
-            if counter==2:
-                break
-        print("|",  end='')
-    stop = time.time()
-    print(f" {(stop-start)/60} mins")
+    patient_dir = f"Y:\{patient_dir}"
+    print(patient_dir)
+    if not os.path.exists(join(ge_dir)):
+        os.makedirs('my_folder')
+    
