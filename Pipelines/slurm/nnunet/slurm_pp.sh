de6dd8a99829950d8c27bd9f2de93b61e13d8d6e
Kiril Klein <kleinkiril94@gmail.com>
2021-10-20 16:00:43 +0200

    minor

--- Pipelines/slurm/nnunet/slurm_pp.sh
+++ Pipelines/slurm/nnunet/slurm_pp.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 # number of cpus we want to allocate for each program
 #SBATCH --ntasks=1 --cpus-per-task=1
-#SBATCH -p gpu --gres=gpu:gtx1080:2
-#SBATCH --time=00:05:00
+#SBATCH -p gpu --gres=gpu:titanrtx:1
+#SBATCH --time=00:20:00
 ##SBATCH –-output=/home/fjn197/Thesis/Pipelines/slurm/out.out
 # Skipping many options! see man sbatch
 # From here on, we can start our program
