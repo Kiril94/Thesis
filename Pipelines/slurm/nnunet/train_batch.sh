7a49bae2db09735e3bac1fb6ed71cde18a0f3dea
fjn197 <fjn197@a00636.science.domain>
2021-10-21 17:07:31 +0200

    minor

--- Pipelines/slurm/nnunet/train_batch.sh
+++ Pipelines/slurm/nnunet/train_batch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=nnunet
 #SBATCH --partition=gpu
 # number of cpus we want to allocate for each program
-#SBATCH --ntasks=1 --cpus-per-task=1
+#SBATCH --ntasks=1 --cpus-per-task=6
 #SBATCH -p gpu --gres=gpu:titanrtx:1
 #SBATCH --time=01:00:00
 ##SBATCH –-output=/home/fjn197/Thesis/Pipelines/slurm/out.out
 # Skipping many options! see man sbatch
 # From here on, we can start our program
-python3 /home/fjn197/Thesis/Pipelines/nnUNet/nnunet/experiment_planning/nnUNet_train.py 2d nnUNetTrainerV2 Task500_Test fold0
+nnUNet_train 2d nnUNetTrainerV2 Task500_Test 0
