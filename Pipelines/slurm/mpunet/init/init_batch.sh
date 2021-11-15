ee87388facc4d38e0344f23afb309beee8ff0b7d
fjn197 <fjn197@a00636.science.domain>
2021-11-15 13:24:51 +0100

    added slurm scripts

--- Pipelines/slurm/mpunet/init/init_batch.sh
+++ Pipelines/slurm/mpunet/init/init_batch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=nnunet
+#SBATCH --job-name=mpunet
 # number of cpus we want to allocate for each program
 ##SBATCH -N 1
 ##SBATCH --ntasks-per-node=24
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH -p gpu --gres=gpu:titanrtx:1
-#SBATCH --time=48:00:00
+#SBATCH --time=00:10:00
 ##SBATCH –-output=/home/fjn197/Thesis/Pipelines/slurm/out.out
 # Skipping many options! see man sbatch
 # From here on, we can start our program
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task501_MICCAI 0 --npz -c
+mp init_project --name MICCAI_project --root /home/fjn197/Thesis/Pipelines/data/mpunet/ --data_dir /home/fjn197/Thesis/Pipelines/data/mpunet/MPUnet_raw_data/MICCAI
