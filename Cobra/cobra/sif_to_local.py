6211bcdb70b657f9a9e13b093c5af0afa8caf5d0
Kiril Klein <kleinkiril94@gmail.com>
2021-11-15 10:36:01 +0100

    minor

--- Cobra/cobra/sif_to_local.py
+++ Cobra/cobra/sif_to_local.py
@@ -21,7 +21,6 @@ if cobra_dir not in sys.path:
     sys.path.append(cobra_dir)
 from utilities import stats
 
-
 #%% 
 # In[tables directories]
 script_dir = os.path.realpath(__file__)
@@ -47,40 +46,28 @@ patient_dir_dic = pd.Series(
 df_all = pd.read_csv(join(table_dir, "neg_pos.csv"))
 
 #%%
-# In[Get relevant patients and volumes]
-# This batch is not finished yet, however we will first take 
-# the one without t2s, since positive patients have no t2s
-#print("Start with smallest group of patients (1104) dwi, \
-#    flair, t2*, t1, mostly negative patients,")
-#group_list = np.loadtxt(join(download_pat_path, "dwi_flair_t2s_t1.txt"),
-#                                   dtype='str')
 
-#print("For now download the group (1104) dwi, \
-#    flair, swi, t1")
-#group_list = np.loadtxt(join(download_pat_path, "dwi_flair_swi_t1.txt"),
-#                                   dtype='str')
 print('lets download t1 pre post')
 group_list = np.loadtxt(join(base_dir, "data/patient_groups","t1_pre_post.txt"),
                                    dtype='str')
-
-
 df_group = df_all[df_all['PatientID'].isin(group_list)]
-# In case you want to download only specific sequences uncomment next lines
-# rel_seq = ['dwi', 'swi', 't1', 't2', 't2s', 'flair']
-# df_group = df_group[df_group['Sequence'].isin(rel_seq)]
 df_group = df_group.sort_values('PatientID')
 
 #%%
-# In[get index of last patient]
+# In[get index of last patient, remove last volume]
+
 patient_list_group = df_group.PatientID.unique()
-# if you want to start with a specific patient uncomment and set last_patient
-# last_patient = "85590c5af43c362de4ececed060da656" #dwi flair t2s t1
+with open(f"{base_dir}/series_log.txt") as f:
+    series_lines = f.readlines()
+last_series_path = series_lines[0]
+print(last_series_path)
+shutil.rmtree(last_series_path)
 
-
-last_patient = "7313d2750cd4da5d8096c1a1dae6ffcb"
-last_patient_idx = np.where(patient_list_group==last_patient)[0][0]
-#last_patient_idx = 0
+with open(f"{base_dir}/patient_log.txt") as f:
+    lines = f.readlines()
+last_patient_idx = int(lines[-2][6:11]) 
 print(last_patient_idx)
+
 #%%
 # In[move crb]
 crb_dst = join(dst_data_dir, 'dcm')
@@ -109,7 +96,11 @@ for pat in patient_list_group[last_patient_idx:]:
     volumes = df_group[df_group.PatientID==pat]['SeriesInstanceUID']
     print(f"download {len(volumes)} volumes")
     for volume in volumes:
-        volume_dir = volume_dir_dic[volume]
+        try:
+            volume_dir = volume_dir_dic[volume]
+        except:
+            print("volume not in dict")
+            continue
         volume_src = os.path.normpath(f"Y:/{volume_dir}")
         if len(os.listdir(volume_src))==0:
             print('-',  end='')
@@ -118,60 +109,12 @@ for pat in patient_list_group[last_patient_idx:]:
             series_uid = volume_src.split(os.sep)[-1]
             volume_dst = join(crb_dst, patient_dir, series_uid)
             try:
-                shutil.copytree(volume_src, volume_dst)
                 with open(f"{base_dir}/volume_log.txt", mode="w") as f:
                     f.write(volume_dir)
+                shutil.copytree(volume_src, volume_dst)
                 print("|",  end='')
             except Exception as e:
 	            print("ERROR : "+str(e))
     stop = time.time()
     print(f"\n {(stop-start)/60:.3} min")
-
-#%%
-# In[move GE (Akshay)]
-dwi_t2s_gre = np.loadtxt(join(download_pat_path, "ge_dwi_t2s_gre.txt"),
-                                   dtype='str')
-gdtg = df_all[df_all['PatientID'].isin(dwi_t2s_gre)]
-gdtg = gdtg.sort_values('PatientID')
-patient_list_gdtg = gdtg.PatientID.unique()
-batches = []
-for i in range(6):
-    if i<5:
-        batches.append(patient_list_gdtg[i*50:(i+1)*50])
-    else:
-        batches.append(patient_list_gdtg[5*50:])
-print(len(batches[-1]))
-
-    
-#%%
-# In[copy whole tree]
-current_batch = 1
-ge_dir = os.path.normpath("G:\CoBra\Data\GE")
-for i, batch in enumerate(batches[current_batch:]):
-    batch_dir = join(ge_dir, f"batch_{i+current_batch}")
-    for pat in batch:
-        start = time.time()
-        patient_dir = volume_dir_df[
-            stats.check_tags(
-                volume_dir_df, tags=[pat], key='Directory')].iloc[0,1]
-        patient_dir = os.path.normpath(patient_dir)
-        patient_dir = join(*list(patient_dir.split(os.sep)[:2]))
-        patient_dir = f"Y:\{patient_dir}"
-        _, patient_id = split(patient_dir)
-        dst_dir = join(batch_dir, patient_id)
-        if exists(dst_dir):
-            print(f"{dst_dir} exists" )
-            continue
-        print(f"{patient_dir}\n->{dst_dir}")
-        shutil.copytree(patient_dir, dst_dir)
-        stop = time.time()
-        print(f" {(stop-start)/60} mins")
-#%%
-# In[Save metadata]
-ge_meta = df_all[df_all.PatientID.isin(patient_list_gdtg)]
-ge_meta['batch'] = None
-for i, batch in enumerate(batches):
-    ge_meta.loc[ge_meta.PatientID.isin(batch), 'batch'] = i
-ge_meta.to_csv("G:\CoBra\Data\GE\metadata.csv",
-               index=False,encoding='utf-8-sig')
     
