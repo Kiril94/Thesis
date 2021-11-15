90769e240c83b498a9f5cc901f2b89698d02f83d
Kiril Klein <kleinkiril94@gmail.com>
2021-11-15 09:52:30 +0100

    copied file

--- Cobra/cobra/utilities/utils.py
+++ Cobra/cobra/utilities/utils.py
@@ -14,8 +14,13 @@ import csv
 from datetime import datetime as dt
 from pathlib import Path
 import logging
+import tarfile
+import os.path
 
 
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 def write_csv(csv_path, patient_list, append=False):
     """This function takes a patient list with patient directories
