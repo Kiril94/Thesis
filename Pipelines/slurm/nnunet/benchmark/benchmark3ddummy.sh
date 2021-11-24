e669debf43f71c230d68984155362a994371499a
Kiril Klein <kleinkiril94@gmail.com>
2021-11-24 12:45:02 +0100

    removed subdir

--- Pipelines/slurm/nnunet/benchmark/benchmark3ddummy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=nnunet_benchmark
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH -p gpu --gres=gpu:titanx:1
-#SBATCH --time=00:10:00
-
-nnUNet_train 3d_fullres nnUNetTrainerV2_5epochs_dummyLoad Task501_MICCAI 0
