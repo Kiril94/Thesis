b087196eb044b56bedbd38a55a9510bb75db846b
Kiril Klein <kleinkiril94@gmail.com>
2021-12-28 18:58:36 +0100

    problem solved, create visualization

--- cobra/minimize_distances.py
+++ cobra/minimize_distances.py
@@ -39,5 +39,87 @@ nx.draw_networkx(
     ax=ax)
 fig.tight_layout()
 #%%
+G = nx.DiGraph()
 
-print(colors)
+G.add_edges_from(
+    [
+        (1, 2, {"capacity": 1, "weight": 0}),
+        (1, 3, {"capacity": 1, "weight": 0}),
+        (1, 4, {"capacity": 1, "weight": 0}),
+        (1, 5, {"capacity": 1, "weight": 0}),
+        (1, 6, {"capacity": 1, "weight": 0}),
+
+        (2, 7, {"capacity": 1, "weight": 9}),
+        (2, 8, {"capacity": 1, "weight": 10}),
+        (3, 7, {"capacity": 1, "weight": 10}),
+        (3, 8, {"capacity": 1, "weight": 20}),
+        (4, 7, {"capacity": 1, "weight": 30}),
+        (4, 8, {"capacity": 1, "weight": 22}),
+        (5, 7, {"capacity": 1, "weight": 21}),
+        (5, 8, {"capacity": 1, "weight": 30}),
+        (6, 7, {"capacity": 1, "weight": 11}),
+        (6, 8, {"capacity": 1, "weight": 40}),
+        
+        (7, 9, {"capacity": 2, "weight": 0}),
+        (8, 9, {"capacity": 2, "weight": 0}),
+    ]
+)
+
+mincostFlow = nx.max_flow_min_cost(G, 1, 9)
+mincost = nx.cost_of_flow(G, mincostFlow)
+print(mincost)
+print(mincostFlow)
+# %%
+#Draw graph
+num_negatives = 5
+num_positives = 3
+fig, ax = plt.subplots()
+BG = nx.Graph()
+source = ['s']
+negatives = np.arange(num_negatives)
+positives = np.arange(num_negatives, 
+                (num_negatives+num_positives))
+sink = ['t']
+
+BG.add_nodes_from(source, bipartite=0)
+BG.add_nodes_from(negatives, bipartite=1)
+BG.add_nodes_from(positives, bipartite=2)
+BG.add_nodes_from(sink, bipartite=3)
+source_negatives_edges = []
+negatives_positives_edges = []
+positives_sink_edges = []
+for neg in negatives:
+    source_negatives_edges.append(('s', neg))
+for neg in negatives:
+    for pos in positives:
+        negatives_positives_edges.append((neg, pos))
+for pos in positives:
+    positives_sink_edges.append((pos, 't'))
+
+BG.add_edges_from(source_negatives_edges)
+BG.add_edges_from(negatives_positives_edges)
+BG.add_edges_from(positives_sink_edges)
+
+
+nodes = BG.nodes()
+# for each of the parts create a set 
+
+nodes_0  = set([n for n in nodes if  BG.nodes[n]['bipartite']==0])
+nodes_1  = set([n for n in nodes if  BG.nodes[n]['bipartite']==1])
+nodes_2  = set([n for n in nodes if  BG.nodes[n]['bipartite']==2])
+nodes_3  = set([n for n in nodes if  BG.nodes[n]['bipartite']==3])
+
+# set the location of the nodes for each set
+pos = dict()
+pos.update( (n, (1, i+int(num_negatives/2))) for i, n in enumerate(nodes_0) ) # put nodes from X at x=1
+pos.update( (n, (2, i)) for i, n in enumerate(nodes_1) ) # put nodes from Y at x=2
+pos.update( (n, (3, i+1)) for i, n in enumerate(nodes_2) ) # put nodes from X at x=1
+pos.update( (n, (4, i+int(num_negatives/2))) for i, n in enumerate(nodes_3) )
+
+# set the colors
+colors = ['tab:orange']
+colors = colors + ['tab:blue' for i in range(num_negatives)]
+colors = colors + ['tab:red' for i in range(num_positives)]
+colors.append('tab:orange')
+nx.draw_networkx(BG, pos=pos, ax=ax,
+    node_color=colors,)
