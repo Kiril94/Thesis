baef0f110ca26a0ea572ff099aa6c763eb548f69
Kiril Klein <kleinkiril94@gmail.com>
2022-01-16 18:05:16 +0100

    function to compute slice distances, caution: still tested, stops after few patients and dicoms, implement the case where no slice location along axis is given

--- cobra/comp_dist_between_slices.py
+++ cobra/comp_dist_between_slices.py
@@ -18,29 +18,40 @@ table_dir = join(base_dir, 'data/tables')
 
 def get_slice_location(dcm_dir):
     dcm = dcmread(dcm_dir)
-    return dcm['SliceLocation']
+    return dcm['SliceLocation'].value
+def get_distances_for_series(series_dir):
+    dcm_dirs = list_subdir(series_dir)
+    try:
+        locations = []
+        for dcm_dir in dcm_dirs[:4]:
+            location = get_slice_location(dcm_dir)
+            locations.append(location) 
+        locations = np.array(locations)
+        loc_diff = np.ediff1d(np.sort(locations))
+        slice_dist = np.median(loc_diff)
+        return slice_dist
+    except: return None
 
 def save_distance_between_slices(sids, 
     dicoms_base_dir='Y:/'):
     """Save missing tags to text."""
     with open(join(base_dir, 'data/t1_longitudinal/sif_dir.json'), 'r') as fp:
         sif_dir_dic = json.load(fp)
-    for i, sid in enumerate(sids[:1]):
-        series_dir = join(dicoms_base_dir, sif_dir_dic[sid])
-        dicom_dirs = list_subdir(series_dir)
-        with multiprocessing.Pool(2) as pool:
-            locations = pool.map(get_slice_location, dicom_dirs)
-        return locations
+    series_dirs = [join(dicoms_base_dir, sif_dir_dic[sid]) for sid in sids]
+    with multiprocessing.Pool(4) as pool:
+                distances = pool.map(get_distances_for_series, series_dirs[:4])
+    return distances
 
 
 def main():
     with open(join(base_dir, 'data/t1_longitudinal/sim_3dt1_sids.json'), 'rb') as f:
         sids_3dt1 = json.load(f)
-    locations = save_distance_between_slices(sids_3dt1)
-    print(locations)
+    distances = save_distance_between_slices(sids_3dt1)
+    return distances
     
 if __name__=="__main__":
     start=time.time()
-    main()
+    slice_distances = main()
+    print(slice_distances)
     print('finished')
     print(time.time()-start)
