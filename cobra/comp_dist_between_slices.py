b57dae135424361e5f4fb9c9706f5b684dba0b45
Kiril Klein <kleinkiril94@gmail.com>
2022-01-22 18:54:07 +0100

    return was changed to write to file, some print statements were removed

--- cobra/comp_dist_between_slices.py
+++ cobra/comp_dist_between_slices.py
@@ -1,3 +1,4 @@
+from email.mime import base
 from os.path import join, split, normpath
 import os
 from utilities.basic import list_subdir
@@ -8,7 +9,7 @@ import json
 import time
 import sys
 from pathlib import Path
-
+from functools import partial
 
 def get_value_from_header(dcm_dir, key):
     dcm = dcmread(dcm_dir)
@@ -77,55 +78,67 @@ def compute_dist_from_img_pos_and_orientation(dcm_dirs, aggregating_func):
     dist_between_slices = np.ediff1d(dist_arr_sorted)
     return dist_between_slices
 
+def write_dist_to_file(write_file, series_dir, dist):
+    with open(write_file,"a+") as file:
+        file.write(f"{series_dir} {dist}\n")
 
-def get_distances_for_series(series_dir, aggregating_func=np.min):
+def get_distances_for_series(series_dir, 
+            aggregation_func, write_file_dir):
+    current_proc = multiprocessing.current_process()    
+    current_proc_id = int(current_proc._identity[0])
+    write_file = join(write_file_dir, f'slice_dist_{current_proc_id}.txt')
+    sid = split(series_dir)[1]
     try:
         dcm_dirs = list_subdir(series_dir)
     except Exception as e:
         print("ERROR : "+str(e))
-        return None
     sys.stdout.flush()
     try:
-        return compute_dist_from_slice_location(dcm_dirs, aggregating_func)
+        dist = compute_dist_from_slice_location(dcm_dirs, aggregation_func)
+        write_dist_to_file(write_file, sid, dist)
     except:
         try:
-            return compute_dist_from_img_pos_and_orientation(dcm_dirs, aggregating_func) 
-        except: return None
-
+            dist = compute_dist_from_img_pos_and_orientation(
+                dcm_dirs, aggregation_func)
+            write_dist_to_file(write_file_dir, sid, dist)
+        except: pass
+    
 def save_distance_between_slices(sids, 
-    dicoms_base_dir='F:/'):
+    dicoms_base_dir='F:/', num_of_procs=8, 
+    write_file_dir="", aggregation_func=np.min):
     """Save missing tags to text."""
     with open(join(base_dir, 'data/t1_longitudinal/sif_dir.json'), 'r') as fp:
         sif_dir_dic = json.load(fp)
     patient_dirs = [join(*normpath(sif_dir_dic[sid]).split(os.sep)[:2]) for sid in sids]
-    print(patient_dirs[0])
     series_dirs = [join(dicoms_base_dir, patient_dirs[i], sid ) \
                 for i, sid in enumerate(sids)]
-    
-    with multiprocessing.Pool(2) as pool:
-                distances = pool.map(get_distances_for_series, 
+    get_distances_for_series_partial = partial(
+        get_distances_for_series,
+        aggregation_func=aggregation_func, write_file_dir=write_file_dir) 
+    with multiprocessing.Pool(num_of_procs) as pool:
+                pool.map(get_distances_for_series_partial, 
                         series_dirs)
-    return distances
 
-
-
+def main(num_of_procs, write_file_dir, aggregation_func=np.min):
+    with open(join(base_dir, 'data/t1_longitudinal/sim_3dt1_sids.json'), 'rb') as f:
+        sids_3dt1 = json.load(f)
+    save_distance_between_slices(sids_3dt1[-30:], 
+            dicoms_base_dir=dicom_base_dir, num_of_procs=num_of_procs, 
+            aggregation_func=aggregation_func, write_file_dir=write_file_dir)
+    return 0
 
 script_dir = os.path.realpath(__file__)
 base_dir = join(Path(script_dir).parent, 'cobra')
 dicom_base_dir = "F:/CoBra/Data/dcm"
-def main():
-    with open(join(base_dir, 'data/t1_longitudinal/sim_3dt1_sids.json'), 'rb') as f:
-        sids_3dt1 = json.load(f)
-    distances = save_distance_between_slices(sids_3dt1[-10:], 
-            dicoms_base_dir=dicom_base_dir )
-    return distances
-    
+write_file_dir = join(base_dir, 
+        'data/t1_longitudinal/distance_between_slices')
+
 if __name__=="__main__":
     start=time.time()
-    slice_distances = main()
-    print(slice_distances)
+    return_status = main(4, write_file_dir)
     print('finished')
-    print(time.time()-start)
+    print(f'status: {return_status}')
+    print(f'Time (min): {(time.time()-start)/60:.3f}')
 
 # For n images, how many images m can be missing
 # in order to compute the slice distance: 
