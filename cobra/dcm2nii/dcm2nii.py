5836b41a4d15f2002c63c78d9260be7a2a4f6344
Kiril <kleinkiril94@gmail.com>
2022-01-25 15:58:26 +0100

    added some description to dcm2nii.py, debugging of move_files_for_pred.py

--- cobra/dcm2nii/dcm2nii.py
+++ cobra/dcm2nii/dcm2nii.py
@@ -14,11 +14,13 @@ base_dir = Path(script_dir).parent.parent
 dcm2nii_exe_path = os.path.join(
     base_dir, "dcm2nii\\dcm2niix_win\\dcm2niix.exe")
 
+def convert_dcm2nii_help():
+     os.system(f"cmd /k {dcm2nii_exe_path} -h")
 
-def convert_dcm2nii(dcm_path, out_path, compression=3, verbose=0, op_sys=0):
+def convert_dcm2nii(dcm_path, out_path, compression=3, verbose=0, op_sys=0,):
     """Given dicom path and output path, converts dicom files in 1 folder
     to a nii file + json file containing the header. 
-    op_sys: select 0 for Windows or 1 for Linux'
+    op_sys: 0 for Windows, 1 for Linux'
     Compression level 3 gives the best time-size tradeoff
     The file name is constructed as follows:
         PatientId_ProtocolName_SequenceName(0018,1020)_SequenceName(0018,0024) 
@@ -27,6 +29,22 @@ def convert_dcm2nii(dcm_path, out_path, compression=3, verbose=0, op_sys=0):
             under which the Series was performed.
         SequenceName (0018,0024):
             so a T1 scan converted with "myName%z" might yield "myNameT1"
+    Main arguments:
+    -f : filename (%a=antenna (coil) name, %b=basename, %c=comments, 
+        %d=description, %e=echo number, %f=folder name, %g=accession number, 
+        %i=ID of patient, %j=seriesInstanceUID, %k=studyInstanceUID, 
+        %m=manufacturer, %n=name of patient, %o=mediaObjectInstanceUID, 
+        %p=protocol, %r=instance number, %s=series number, %t=time, 
+        %u=acquisition number, %v=vendor, %x=study ID; %z=sequence name; 
+        default '%f_%p_%t_%s')
+    
+    Other arguments:
+        -a : adjacent DICOMs (images from same series always in same folder) 
+            for faster conversion (n/y, default n)
+        -l : losslessly scale 16-bit integers to use dynamic range (y/n/o 
+            [yes=scale, no=no, but uint16->int16, o=original], default n)
+        -z : gz compress images (y/i/n/3, default n)
+        --progress : report progress (y/n, default n)
         """
     if (op_sys == 0):  # WINDOWS
         os.system(f"cmd /k {dcm2nii_exe_path} -{compression} -a y\
--- cobra/move_files_for_pred.py
+++ cobra/move_files_for_pred.py
@@ -5,9 +5,15 @@ from os.path import join
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import gzip
+import multiprocessing as mp
+from dcm2nii import dcm2nii
 
 #%%
-disk_dir = "F:"
+disk_data_dir = join("F:\\", 'CoBra', 'Data')
+pos_nii_dir = join(disk_data_dir, 'nii', 'positive')
+pred_input_dir = join(disk_data_dir, 'volume_longitudinal', 'input')
+
 script_dir = os.path.realpath(__file__)
 base_dir = Path(script_dir).parent
 data_dir = join(base_dir, 'data')
@@ -24,6 +30,36 @@ df_cases = df3dt1[df3dt1.PatientID.isin(cases_ls)]
 df_pos_no_cases = df3dt1[~df3dt1.PatientID.isin(cases_ls)]
 
 # %%
-source_target_dirs = [
-    (join(), target) for 
-    ]
+def get_source_target_dirs(df, base_src_dir, 
+            base_tgt_dir):
+    return [
+        (join(base_src_dir, row.PatientID, row.SeriesInstanceUID+'.nii'),
+    join(base_tgt_dir, row.PatientID+row.SeriesInstanceUID+'.nii.gz'))\
+    for _, row in df.iterrows()]  
+
+
+pat_sids_cases_src_tgt_dirs = get_source_target_dirs(
+    df_cases, base_src_dir=pos_nii_dir, 
+    base_tgt_dir=join(pred_input_dir, 'cases') )
+pat_sids_pos_no_cases_src_tgt_dirs = get_source_target_dirs(
+    df_pos_no_cases, base_src_dir=pos_nii_dir, 
+    base_tgt_dir=join(pred_input_dir, 'positive_cases_excluded') )
+
+src_tgt_ls = pat_sids_cases_src_tgt_dirs + \
+    pat_sids_pos_no_cases_src_tgt_dirs
+
+#%%
+dcm2nii.convert_dcm2nii_help()
+#%%
+def move_and_gz_files(src_tgt):
+        with open(src_tgt[0], 'rb') as f_in:
+            with gzip.open(src_tgt[1], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+def main(source_target_list, procs=8):
+    with mp.Pool(procs) as pool:
+                pool.map(move_and_gz_files, 
+                        source_target_list)
+
+if __name__ == '__main__':
+    main(src_tgt_ls[1:5], procs=2)
