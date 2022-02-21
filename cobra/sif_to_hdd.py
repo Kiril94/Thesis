d9caa68feb168ba53dc307cc7036befa156ce859
Kiril Klein <kleinkiril94@gmail.com>
2022-02-21 11:53:34 +0100

    creating new ids

--- cobra/sif_to_hdd.py
+++ cobra/sif_to_hdd.py
@@ -65,7 +65,7 @@ group_list = list(set(group_list).difference(set(downloaded_ls)))
 print("Volumes still to download: ", len(group_list))
 use_batches = True
 if use_batches:
-    batch = 10
+    batch = 0
     print("batch:", {batch})
     start = 0
     batch_size = 1000
