246a37c6b22f8f33f5faa6752402dde4d8beb7a7
Kiril Klein <kleinkiril94@gmail.com>
2022-02-22 16:24:34 +0100

    running final tests

--- cobra/move_files_for_pred_mat.py
+++ cobra/move_files_for_pred_mat.py
@@ -1,88 +1,86 @@
 #%%
 import shutil
-import sys
 import os
 from os.path import join, split
 from pathlib import Path
-import pandas as pd
-import numpy as np
-import gzip
 import multiprocessing as mp
-from dcm2nii import dcm2nii
-from datetime import datetime as dt
 import time
 import json
 import pickle
-from utilities import basic, fix_dcm_incomplete_vols
-from pydicom import dcmread
-from utilities.basic import get_dir, list_subdir, make_dir, remove_file, get_part_of_path, get_proc_id
+from utilities.basic import list_subdir, make_dir, get_proc_id
 import matlab.engine
-
+from functools import partial
+# paths
 script_dir = os.path.realpath(__file__)
 base_dir = Path(script_dir).parent
 tables_dir = join(base_dir, 'data', 'tables')
 disk_data_dir = join("F:\\", 'CoBra', 'Data')
-nii_input_dir = join(disk_data_dir, 'volume_longitudinal_nii', 'input', 'nii_files')
-
-
+tgt_dir = join(disk_data_dir, 'volume_longitudinal_nii', 'input', 'nii_files')
+tmp_dir = join(disk_data_dir, 'volume_longitudinal_nii', 'temp')
+# matlab engine
 eng = matlab.engine.start_matlab()
 eng.addpath('C:\\Users\\kiril\\Thesis\\CoBra\\cobra\\dcm2nii\\dcm2nii_mat\\functions', nargout=0)
 eng.addpath('C:\\Users\\kiril\\Thesis\\CoBra\\cobra\\dcm2nii\\dcm2nii_mat\\dcm2nii')
 eng.addpath('C:\\Users\\kiril\\Thesis\\CoBra\\cobra\\dcm2nii\\dcm2nii_mat\\spm12')
-
-
+# load necessary files
 with open(join(tables_dir, 'newIDs_dic.pkl'), 'rb') as f:
     id_dic = pickle.load(f)
-
 with open("C:\\Users\\kiril\\Thesis\\CoBra\\cobra\\data\\t1_longitudinal\\pairs_3dt1_long_sids.pkl", 'rb') as f:
     sids_ls = pickle.load(f)
-
 with open("C:\\Users\\kiril\\Thesis\\CoBra\\cobra\\data\\tables\\disk_series_directories.json", 'rb') as f:
     dir_dic = json.load(f)
 
-
-def get_missing_files(sids_to_download, download_dir, newid_dic):
+# define functions
+def get_missing_files(sids_to_conv, nii_dir, newid_dic):
     """
-    sids_to_download: List of SeriesInstanceUIDs that need to be downloaded
-    download_dir: str, directory where downloaded files are placed
+    sids_to_conv: List of SeriesInstanceUIDs that need to be converted to nii
+    nii_dir: str, directory where converted files are placed
     newid_dic: dictionary used to map sids to 6 digit new ids
     returns: list of missing files sids
     """
     inv_map = {v: k for k, v in newid_dic.items()}
-    downloaded_files_ids = [file[:-7] for file in os.listdir(download_dir)]
-    downloaded_files_sids = [inv_map[id] for id in downloaded_files_ids]
-    missing_files = set(sids_to_download).difference(set(downloaded_files_sids))
+    conv_files_ids = [file[:-7] for file in os.listdir(nii_dir)]
+    conv_files_sids = [inv_map[id] for id in conv_files_ids]
+    missing_files = set(sids_to_conv).difference(set(conv_files_sids))
     return list(missing_files)
-
-def dcm2nii_mat_main(src_dir, tmp_dir, tgt_dir, tgt_name):
-    """src_dir: """
-    tmp_dir_sp = join(tmp_dir, get_proc_id())
+    
+def dcm2nii_mat(src_dir, tgt_path, tmp_dir, test=False):
+    """Converts dcm to nii using dcm2nii (matlab) or spm12 (matlab) if first fails
+    src_dir: Directory with dcm series
+    tgt_path: Full path of the nii file that will be produced (should end with .nii.gz)"""
+    tmp_dir_sp = join(tmp_dir, str(get_proc_id(test)))
     make_dir(tmp_dir_sp)
     try:
-        eng.dcm2nii_main(src_dir, tmp_dir)
+        eng.dcm2nii_main(src_dir, tmp_dir_sp)
     except:
+        shutil.remove()
         print("dcm2nii failed, try spm")
         try:
-            eng.spm12_main(src_dir, tmp_dir)
+            eng.spm12_main(src_dir, tmp_dir_sp)
         except:
-            for f in list_subdir(tmp_dir):
+            for f in list_subdir(tmp_dir_sp):
                 shutil.remove(f)
             print('x')
-    out_files = list_subdir(tmp_dir, ending='.nii.gz')
-    assert len(out_files)==0, f'More than 1 nii file was created for {src_dir}'
-    shutil.move(out_files[0], join(tgt_dir, tgt_name+'.nii.gz'))
+    out_files = list_subdir(tmp_dir_sp, ending='.nii.gz')
+    assert len(out_files)<=1, f'More than 1 nii file was created for {src_dir}'
+    if len(out_files)==1:
+        shutil.move(out_files[0], tgt_path)
+    else:
+        pass
     return 0
+def dcm2nii_mat_main(sids_ls, id_dic, tmp_dir, tgt_dir, test=False):
+    """sids_ls: List of sids that need to be converted"""
+    missing_files = get_missing_files(sids_ls, tgt_dir, id_dic)
+    if test:
+        missing_files = missing_files[10:12]
+    sids = [split(f)[1] for f in missing_files]
+    tgt_paths = [join(tgt_dir, id_dic[sid]+'.nii.gz') for sid in sids]
+    src_dirs = [dir_dic[sid] for sid in sids]
+
+    mp_input = [(src_dir, tgt_path) for src_dir, tgt_path in zip(src_dirs, tgt_paths)]
+    dcm2nii_mat_p = partial(dcm2nii_mat, tmp_dir=tmp_dir)
+    with mp.Pool() as pool:
+                pool.starmap(dcm2nii_mat_p, mp_input)
 
 if __name__ == '__main__':
-    #dcm2nii_mat_main()
-    src_test = "F:\CoBra\Data\dcm\\2019_06\\00a4185a2d35d0fb20f16747fa8b9d36\\7c7bff9d964225ac568dea16f5b69987"
-    tgt_test = "F:\\CoBra\\Data\\test"
-    missing_files = get_missing_files(sids_ls, nii_input_dir, id_dic)
-    print(len(missing_files))
-    #print(missing_files[:2])
-    print(len(sids_ls))
-    for f in missing_files[:3]:
-        sid = split(f)[1]
-        new_id = id_dic[sid]
-        src_dir = dir_dic[sid]
-        dcm2nii_mat_main(src_dir, tgt_test, tgt_test, new_id)
+    dcm2nii_mat_main(sids_ls, id_dic, tmp_dir, tgt_dir, test=True)
