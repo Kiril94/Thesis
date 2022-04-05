a099d4306e8099b7ad885f9dd4eaa486324292f4
Kiril Klein <kleinkiril94@gmail.com>
2022-04-05 17:10:14 +0200

    minor

--- cobra/t1_long/01_clean_df.py
+++ cobra/t1_long/01_clean_df.py
@@ -2,14 +2,11 @@
 import os, sys
 import pickle
 from os.path import split, join
-
-from sklearn.metrics import confusion_matrix
 import gzip
 base_dir = split(os.getcwd())[0]
 if base_dir not in sys.path:
     sys.path.append(base_dir)
 from pathlib import Path
-import seaborn as sns
 from utilities import utils
 import datetime
 import pandas as pd
@@ -19,7 +16,7 @@ import numpy as np
 script_dir = os.path.realpath(__file__)
 base_dir = Path(script_dir).parent.parent
 table_dir = f"{base_dir}/data/tables/scan_tables"
-dfc = pd.read_feather(join(base_dir,'data','tables', 'neg_pos_clean'))
+# dfc = pd.read_feather(join(base_dir,'data','tables', 'neg_pos_clean'))
 with open(join(table_dir, 'scan_after_sq_pred.pkl'), 'rb') as f:
     df = pickle.load(f)
 df1 = df[df.Sequence=='t1']
@@ -43,6 +40,8 @@ df1_0.InstanceCreationDate.hist()
 mask = (df1_0.InstanceCreationDate.isna()) & (df1_0['2019']==1)
 df1_0['InstanceCreationDate'] = np.where(
     mask, datetime.datetime(2018, 1, 1), df1_0['InstanceCreationDate'])
+
+
 #%%
 # In[Get number of slices if missing]
 print("We need the number of slices for scans that have as MRAcquisitionType either nan or 3D")
@@ -62,23 +61,29 @@ with open('nos.pkl', 'rb') as f:
     n_slices_dic = pickle.load(f)
 
 df1_0.NumberOfSlices.fillna(pd.Series(n_slices_dic), inplace=True)
-print (df1_0)
+
 #%%
-# In[Keep scan if 3D or if missing if number of slices>64]
+# In[Keep scan if 3D or if missing if number of slices>=64]
 #df1_1 = df1_0[df1_0.MRAcquisitionType.isna() ].NumberOfSlices.hist()
 mask3d = (df1_0.MRAcquisitionType.isna() | (df1_0.MRAcquisitionType=='3D'))\
     & (df1_0.NumberOfSlices>=64)
 df3d = df1_0[mask3d]
-with gzip.open(join(table_dir, 'scan_3d.gz'), 'wb') as f:
+df3d = df3d.reset_index()
+
+#%%
+
+with gzip.open(join(table_dir, 'scan_3dt1_clean.gz'), 'wb') as f:
     pickle.dump(df3d, f)
-
-
+#%%
+df3d.to_csv(join(table_dir, 'scan_3dt1_clean.csv'), index=False, header=True)
+#%%
+df3d.keys()
 #%%
 #df[df1_1.NumberOfSlices<200].NumberOfSlices.hist(bins=20)
 print(len(df3d),'3d scans')
 df3d.DistanceBetweenSlices.isna().mean()
-dfc_3dt1 = dfc[(dfc.NumberOfSlices>=64) & (dfc.MRAcquisitionType=='3D')]
-df_new_pat = df3d[~df3d.PatientID.isin(dfc_3dt1.PatientID)]
+# dfc_3dt1 = dfc[(dfc.NumberOfSlices>=64) & (dfc.MRAcquisitionType=='3D')]
+# df_new_pat = df3d[~df3d.PatientID.isin(dfc_3dt1.PatientID)]
 
 #%%
 print('3D T1 patients')
