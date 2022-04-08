367facffcfbe2fddb784cc8c251b161f6ca6b881
Kiril Klein <kleinkiril94@gmail.com>
2022-04-08 18:57:20 +0200

    script for augmentation pipeline

--- cobra/augmentation/augment_nii.py
+++ cobra/augmentation/augment_nii.py
@@ -6,20 +6,48 @@ if base_dir not in sys.path:
     sys.path.append(base_dir)
 from pathlib import Path
 import nibabel as nib
+from utilities import basic
+nib.Nifti1Header.quaternion_threshold = -1e-06
 #%%
 thesis_dir = Path(base_dir).parent
 aug_dir = join(thesis_dir, 'MRI-Augmentation')
+data_dir = join(base_dir, 'data')
 #%%
 import matlab.engine
 eng = matlab.engine.start_matlab()
-eng.addpath(join(aug_dir, '3D', 'bias_field'), nargout=0)
-eng.addpath(join(aug_dir, '3D', 'elastic_deform'), nargout=0)
-eng.addpath(join(aug_dir, '3D', 'gibbs_ringing'), nargout=0)
-eng.addpath(join(aug_dir, '3D', 'motion_ghosting'), nargout=0)
+eng.addpath(join(aug_dir, '3D'), nargout=0)
+eng.addpath(join(aug_dir, '3D', 'bias_field'))
+eng.addpath(join(aug_dir, '3D', 'elastic_deform'))
+eng.addpath(join(aug_dir, '3D', 'gibbs_ringing'))
+eng.addpath(join(aug_dir, '3D', 'motion_ghosting'))
 #%%
 # In[Load test image]
-img = nib.load("F:\\CoBra\\Data\\volume_longitudinal_nii\\input\\nii_files\\006725.nii.gz")
-X = img.get_fdata()
+img_dir = join(data_dir, "augmentations\\MICCAI\\imagesTr")
+lbl_dir = join(data_dir,"augmentations\\MICCAI\\labelsTr")
+imga_dir = join(data_dir, "augmentations\\MICCAI\\imagesTr_aug")
+lbla_dir = join(data_dir,"augmentations\\MICCAI\\labelsTr_aug")
+tst_img = nib.load(basic.list_subdir(img_dir)[0])
+tst_lbl = nib.load(basic.list_subdir(lbl_dir)[0])
+
+#%%
+# In[Lets try for one img]
+X = tst_img.get_fdata()
+aff = tst_img.affine
 #%%
 # In[Try augmentation]
-eng.bias_field()
+def normalize(X):
+    xmin, xmax = X.min(), X.max()
+    return (X-xmin)/(xmax-xmin), xmin, xmax
+def renormalize(X, xmin, xmax):
+    return X*(xmax-xmin)+xmin
+Xn, xmin, xmax = normalize(X)
+X1 = eng.bias_field(Xn)
+
+#%%
+Xorg = renormalize(Xn, xmin,xmax)
+tst_img = nib.Nifti1Image(Xorg, aff)
+# eng.bias_field()
+nib.save(tst_img, join(imga_dir, 'test.nii.gz'))
+
+#%%
+print(1-0.2**4)
